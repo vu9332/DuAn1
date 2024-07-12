25c03713e06b44a62045fcc2380880f749b394ad
phamviethungbmt <phamviethungbmt@gmail.com>
2024-07-12 19:26:02 +0700

    map test

---
+++ Assets/parallax.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class parallax : MonoBehaviour
+{
+    Material mat;
+    float distance;
+
+    [Range(0f, 0.5f)]
+    public float speed = 0.02f;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+    private void Update()
+    {
+        distance += Time.deltaTime;
+        mat.SetTextureOffset("_MainTex", Vector2.right * distance);
+    }
+}

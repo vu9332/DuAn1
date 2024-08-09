3acb64970b71545676176856061b18cb0ffe6c7c
tdlm987 <trungdlmpk03822@gmail.com>
2024-08-09 15:34:03 +0700

    Them animation cho Coins

--- Assets/TrungWork/Scripts/Gold.cs
+++ Assets/TrungWork/Scripts/Gold.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.Rendering;
 
 public class Gold : MonoBehaviour
 {
     [SerializeField] private PlayerData playerData;
     [SerializeField] private float speed;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private GameObject imageCoinsObject;
+    [SerializeField] float duration;
+    [SerializeField] private Ease easeCoin;
+    private Transform posEnd;
     private Vector3 posTarget;
     private Vector3 gameObjectPos;
     private bool isCollectedCoin=false;
@@ -16,14 +21,9 @@ public class Gold : MonoBehaviour
     private Collider2D coll;
     private void Awake()
     {
+        posEnd = GameObject.Find("CoinImage").GetComponent<RectTransform>();
         coll=GetComponent<Collider2D>();
         rb=GetComponent<Rigidbody2D>();
-    }
-    private void Start()
-    {
-        RectTransform rectPosition= GameObject.Find("CoinImage").GetComponent<RectTransform>();
-        posTarget = rectPosition.position;
-        gameObjectPos= Camera.main.WorldToScreenPoint(transform.position);
     }
     private void Update()
     {
@@ -36,17 +36,29 @@ public class Gold : MonoBehaviour
     {
         if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
-            CharacterEvents.characterTookItem.Invoke(UIManager.UIManagerInstance.coinTextPrefab,gameObject, 1);
-            AudioManager.Instance.PlaySoundSFX(AudioManager.Instance.snd_pick_up);
+            CharacterEvents.characterTookItem.Invoke(UIManager.UIManagerInstance.coinTextPrefab, gameObject, 1);
             playerData.playerCoin = ++playerData.playerCoin;
+            AudioManager.Instance.PlaySoundSFX(AudioManager.Instance.snd_pick_up);
             isCollectedCoin=true;
             rb.bodyType = RigidbodyType2D.Kinematic;
             coll.enabled = false;
+            OnAniamtionCoins();
             Destroy(gameObject);
         }
         if (coll.IsTouchingLayers(groundLayer))
         {
             AudioManager.Instance.PlaySoundSFX(AudioManager.Instance.snd_coin);
+        }
+    }
+    public void OnAniamtionCoins()
+    {
+        Vector2 posCoinsInOverlay=Camera.main.WorldToScreenPoint(transform.position);
+        GameObject coinImage=Instantiate(imageCoinsObject, posCoinsInOverlay, Quaternion.identity);
+        coinImage.GetComponentInChildren<Image>().rectTransform.position = posCoinsInOverlay;
+        coinImage.GetComponentInChildren<Image>().rectTransform.DOMove(posEnd.position, duration).SetEase(easeCoin);
+        if(coinImage.GetComponentInChildren<Image>().rectTransform== posEnd)
+        {
+            Destroy(coinImage);
         }
     }
 }
